89d190cc53f04b304c65bdea57c95906020f41fd
punit patel <153413452+punitpatel550@users.noreply.github.com>
2026-05-07 17:53:59 +0530

    mongodb migration and dealer app update

--- Coupon_System/config.py
+++ Coupon_System/config.py
@@ -3,13 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# ------------------------------
-# MySQL Config
-# ------------------------------
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "@punit1280")
-MYSQL_DB = os.getenv("MYSQL_DB", "coupon_system")
 
 # ------------------------------
 # MongoDB Atlas Config
